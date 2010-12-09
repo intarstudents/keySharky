530039084f8bf92ad2465e1fc3b3890caccd4dd5
Intars <the.mobix@gmail.com>
2010-12-09 20:23:51 +0200

    Fixed keySharky on new HTML Grooveshark
    - Now toggling by injecting javascript (omg Chrome, why o why!)
    - Change in version : 1.5

--- google-chrome/listener.js
+++ google-chrome/listener.js
@@ -1,29 +1,35 @@
 var keysharkyListener = {
   
+  Grooveshark: function(toggle)
+  {
+    // Chrome ... WTF did you made me write?
+    var elem = document.createElement("script");
+    elem.type = "text/javascript";
+    elem.innerHTML = "Grooveshark." + toggle + ";";
+    
+    var append = document.head.appendChild(elem);
+    document.head.removeChild(append);
+  },
+  
   // Init keysharkyListener object
   init: function(){
     
     if (window.location.href.search(/^http\:\/\/(listen|preview|staging|retro)\.grooveshark\.com/) != -1){
       
-      try{
-        this.gsliteswf = document.getElementById('gsliteswf');
-      }catch(e){}
-      
-      //Inject in tab gsliteswf object toggler who is waiting to execute keyup actions
       chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         var allToggles = {
-          "play"      : function(){ keysharkyListener.gsliteswf.togglePlayback(); },
-          "stop"      : function(){ keysharkyListener.gsliteswf.pausePlayback(); },
-          "prev"      : function(){ keysharkyListener.gsliteswf.previous(); },
-          "next"      : function(){ keysharkyListener.gsliteswf.next(); },
+          "play"      : function(){ keysharkyListener.Grooveshark("togglePlayPause()"); },
+          "stop"      : function(){ keysharkyListener.Grooveshark("pause()"); },
+          "prev"      : function(){ keysharkyListener.Grooveshark("previous()"); },
+          "next"      : function(){ keysharkyListener.Grooveshark("next()"); },
           
-          "favorite"  : function(){ keysharkyListener.gsliteswf.favoriteSong(); },
-          "voteup"    : function(){ keysharkyListener.gsliteswf.voteSong(1); },
-          "votedown"  : function(){ keysharkyListener.gsliteswf.voteSong(-1); },
-          "voteclear" : function(){ keysharkyListener.gsliteswf.voteSong(0); },
+          "favorite"  : function(){ keysharkyListener.Grooveshark("favoriteCurrentSong()"); },
+          "voteup"    : function(){ keysharkyListener.Grooveshark("voteCurrentSong(1)"); },
+          "votedown"  : function(){ keysharkyListener.Grooveshark("voteCurrentSong(-1)"); },
+          "voteclear" : function(){ keysharkyListener.Grooveshark("voteCurrentSong(0)"); },
         };
         
-        if (request.method == "gsliteswf" && allToggles[request.action] != undefined){
+        if (request.method == "Grooveshark" && allToggles[request.action] != undefined){
         
           try{
             allToggles[request.action]();
@@ -35,42 +41,41 @@ var keysharkyListener = {
         }
       });
       
-    }else{
-    
-      this.unAllowedKeys = [16, 17, 18, 91];
-      
-      // Inject in tab keyup listener, who will check for (maybe) valid keysharky combo
-      window.addEventListener('keyup', function(event){
-        
-        var modifiers = new Array();
-        var key = '';
-        var keycode = '';
-        
-        // Get the modifiers
-        if(event.metaKey) modifiers.push('meta');
-        if(event.ctrlKey) modifiers.push('control');
-        if(event.altKey) modifiers.push('alt');
-        if(event.shiftKey) modifiers.push('shift');
-        
-        // Get keycode
-        if(event.keyCode) {
-	        keycode = event.keyCode;
-        }
-
-        if(modifiers.length > 0 && !keysharkyListener.inArray(keysharkyListener.unAllowedKeys, keycode)) {
-          
-          var request = {
-            "method" : "keyup",
-            "modifiers" : modifiers,
-            "keycode"   : keycode,
-          };
-          chrome.extension.sendRequest(request, function(response){});
-          
-        }
-        
-      }, false);
-      
     }
+    
+    this.unAllowedKeys = [16, 17, 18, 91];
+    
+    // Inject in tab keyup listener, who will check for (maybe) valid keysharky combo
+    window.addEventListener('keyup', function(event){
+      
+      var modifiers = new Array();
+      var key = '';
+      var keycode = '';
+      
+      // Get the modifiers
+      if(event.metaKey) modifiers.push('meta');
+      if(event.ctrlKey) modifiers.push('control');
+      if(event.altKey) modifiers.push('alt');
+      if(event.shiftKey) modifiers.push('shift');
+      
+      // Get keycode
+      if(event.keyCode) {
+        keycode = event.keyCode;
+      }
+
+      if(modifiers.length > 0 && !keysharkyListener.inArray(keysharkyListener.unAllowedKeys, keycode)) {
+        
+        var request = {
+          "method" : "keyup",
+          "modifiers" : modifiers,
+          "keycode"   : keycode,
+        };
+        chrome.extension.sendRequest(request, function(response){});
+        
+      }
+      
+    }, false);
+      
   },
   
   // Check if Object is Array
