1439c6466c8f96e7b4bab587ecb2144cc12704bf
Intars <the.mobix@gmail.com>
2011-04-26 15:19:18 +0300

    Hotfix: keySharky 1.5.5 for Google Chrome
    - fixes changes of Grooveshark player url from http://listen.grooveshark.com to http://grooveshark.com

--- google-chrome/listener.js
+++ google-chrome/listener.js
@@ -1,87 +1,87 @@
 var keysharkyListener = {
-  
+
   Grooveshark: function(toggle)
   {
     // Chrome ... WTF did you made me write?
     var elem = document.createElement("script");
     elem.type = "text/javascript";
     elem.innerHTML = "Grooveshark." + toggle + ";";
-    
+
     var append = document.head.appendChild(elem);
     document.head.removeChild(append);
   },
-  
+
   // Init keysharkyListener object
   init: function(){
-    
-    if (window.location.href.search(/^http\:\/\/(listen|preview|staging|retro)\.grooveshark\.com/) != -1){
-      
+
+    if (window.location.href.search(/^http\:\/\/((listen|preview|staging|retro)\.|)grooveshark\.com/) != -1){
+
       chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
         var allToggles = {
           "play"      : function(){ keysharkyListener.Grooveshark("togglePlayPause()"); },
           "stop"      : function(){ keysharkyListener.Grooveshark("pause()"); },
           "prev"      : function(){ keysharkyListener.Grooveshark("previous()"); },
           "next"      : function(){ keysharkyListener.Grooveshark("next()"); },
-          
+
           "favorite"  : function(){ keysharkyListener.Grooveshark("favoriteCurrentSong()"); },
           "voteup"    : function(){ keysharkyListener.Grooveshark("voteCurrentSong(1)"); },
           "votedown"  : function(){ keysharkyListener.Grooveshark("voteCurrentSong(-1)"); },
           "voteclear" : function(){ keysharkyListener.Grooveshark("voteCurrentSong(0)"); },
-          
+
           "mute"      : function(){ keysharkyListener.Grooveshark("setIsMuted(Grooveshark.getIsMuted() ? false : true)"); },
           "volup"     : function(){ keysharkyListener.Grooveshark("setVolume(Grooveshark.getVolume() + 10)"); },
           "voldown"   : function(){ keysharkyListener.Grooveshark("setVolume(Grooveshark.getVolume() - 10)"); },
         };
-        
+
         if (request.method == "Grooveshark" && allToggles[request.action] != undefined){
-        
+
           try{
             allToggles[request.action]();
             sendResponse({"result" : 200});
           }catch(e){
             sendResponse({"result" : 500});
           }
-          
+
         }
       });
-      
+
     }
-    
+
     this.unAllowedKeys = [16, 17, 18, 91];
-    
+
     // Inject in tab keyup listener, who will check for (maybe) valid keysharky combo
     window.addEventListener('keyup', function(event){
-      
+
       var modifiers = new Array();
       var key = '';
       var keycode = '';
-      
+
       // Get the modifiers
       if(event.metaKey) modifiers.push('meta');
       if(event.ctrlKey) modifiers.push('control');
       if(event.altKey) modifiers.push('alt');
       if(event.shiftKey) modifiers.push('shift');
-      
+
       // Get keycode
       if(event.keyCode) {
         keycode = event.keyCode;
       }
 
       if(modifiers.length > 0 && !keysharkyListener.inArray(keysharkyListener.unAllowedKeys, keycode)) {
-        
+
         var request = {
           "method" : "keyup",
           "modifiers" : modifiers,
           "keycode"   : keycode,
         };
         chrome.extension.sendRequest(request, function(response){});
-        
+
       }
-      
+
     }, false);
-      
+
   },
-  
+
   // Check if Object is Array
   inArray: function(arr, value){
     var i;
@@ -92,13 +92,13 @@ var keysharkyListener = {
     }
     return false;
   }
-  
+
 }
 
 try{
 
   keysharkyListener.init();
-  
+
 }catch(e){
   // Fail, but with dignity!
 }
