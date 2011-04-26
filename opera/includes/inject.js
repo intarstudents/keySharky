4622e7076d9be926053fc55bf4fd21e5b0468304
Intars <the.mobix@gmail.com>
2011-04-26 15:35:15 +0300

    Hotfix: keySharky 1.5.5 for Opera
    - fixes changes of Grooveshark player url from http://listen.grooveshark.com to http://grooveshark.com

--- opera/includes/inject.js
+++ opera/includes/inject.js
@@ -1,5 +1,5 @@
 var keysharkyListener = {
-  
+
   Grooveshark: function(toggle)
   {
     var elem = window.document.createElement("script");
@@ -11,8 +11,8 @@ var keysharkyListener = {
   },
 
   init: function(){
-    
-    if (window.location.href.search(/^http\:\/\/(listen|preview|staging|retro)\.grooveshark\.com/) != -1 && window.location.href.search(/pixels\.php/) == -1){
+
+    if (window.location.href.search(/^http\:\/\/((listen|preview|staging|retro)\.|)grooveshark\.com/) != -1 && window.location.href.search(/pixels\.php/) == -1){
       window.addEventListener('load', function(event) {
 
         opera.extension.onmessage = function(event) {
@@ -21,12 +21,12 @@ var keysharkyListener = {
             "stop"      : function(){ keysharkyListener.Grooveshark("pause()"); },
             "prev"      : function(){ keysharkyListener.Grooveshark("previous()"); },
             "next"      : function(){ keysharkyListener.Grooveshark("next()"); },
-            
+
             "favorite"  : function(){ keysharkyListener.Grooveshark("favoriteCurrentSong()"); },
             "voteup"    : function(){ keysharkyListener.Grooveshark("voteCurrentSong(1)"); },
             "votedown"  : function(){ keysharkyListener.Grooveshark("voteCurrentSong(-1)"); },
             "voteclear" : function(){ keysharkyListener.Grooveshark("voteCurrentSong(0)"); },
-            
+
             "mute"      : function(){ keysharkyListener.Grooveshark("setIsMuted(Grooveshark.getIsMuted() ? false : true)"); },
             "volup"     : function(){ keysharkyListener.Grooveshark("setVolume(Grooveshark.getVolume() + 10)"); },
             "voldown"   : function(){ keysharkyListener.Grooveshark("setVolume(Grooveshark.getVolume() - 10)"); },
@@ -39,41 +39,41 @@ var keysharkyListener = {
 
       }, false);
     }
-  
+
     this.unAllowedKeys = [16, 17, 18, 91];
-    
+
     // Inject in tab keyup listener, who will check for (maybe) valid keysharky combo
     window.addEventListener('keyup', function(event){
-      
+
       var modifiers = new Array();
       var key = '';
       var keycode = '';
-      
+
       // Get the modifiers
       if(event.metaKey) modifiers.push('meta');
       if(event.ctrlKey) modifiers.push('control');
       if(event.altKey) modifiers.push('alt');
       if(event.shiftKey) modifiers.push('shift');
-      
+
       // Get keycode
       if(event.keyCode) {
         keycode = event.keyCode;
       }
 
       if(modifiers.length > 0 && !keysharkyListener.inArray(keysharkyListener.unAllowedKeys, keycode)) {
-        
+
         var request = {
           "method" : "keyup",
           "modifiers" : modifiers,
           "keycode"   : keycode,
         };
         opera.extension.postMessage(request);
-        
+
       }
-      
+
     }, false);
   },
-  
+
   // Check if Object is Array
   inArray: function(arr, value){
     var i;
@@ -89,7 +89,7 @@ var keysharkyListener = {
 try{
 
   keysharkyListener.init();
-  
+
 }catch(e){
   // Fail, but with dignity!
 }
